a442717c723ad92969eaa1db9006ec30c4f166d6
Andrew Pai <andrew1103@gmail.com>
2018-06-30 16:28:37 -0700

    finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,42 @@
 import os
 import csv
 #path
-budgetCSV = os.path.join("Resources", "budget_data.csv")
-Budget = {}
-Date = []
-Revenue = []
-sum = 0
-#function
-#def getData(budgetData):
-    #totalMonths = len(Date)
-#read
+budgetCSV = os.path.join("budget_data.csv")
 with open(budgetCSV, newline="") as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=",")
-        header  = next(csvreader)
-        for row in csvreader:
-            Date.append(row[0])
-            Revenue.append(row[1])  
-            sum = sum + int(row[1])
-            NetRevenue = sum
-        GreatestIncrease = max(row[1])
-        GreatestDecrease = min(row[1])
-        totalMonths = len(Date)
-#print
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header  = next(csvreader)
+    Date = []
+    Revenue = []
+    RevChange = []
+    for row in csvreader:
+        Date.append(row[0])
+        Revenue.append(float(row[1]))
+print("Financial Analysis")
+print("____________________________________")
+print("Total Months: ", len(Date))
+print("Total Revenue: ", sum(Revenue))
+for i in range(1,len(Revenue)):
+    RevChange.append(Revenue[i] - Revenue[i-1])
+    AvgRevChange = str(sum(RevChange) / len(RevChange))
+    MaxRevChange = str(max(RevChange))
+    MinRevChange = str(min(RevChange))
+    MaxRevChangeDate = str(Date[RevChange.index(max(RevChange))])
+    MinRevChangeDate = str(Date[RevChange.index(min(RevChange))])
+print("Average Revenue Change: $", AvgRevChange)
+print("Greatest Increase in Revenue:",MaxRevChangeDate, "($",MaxRevChange,")")
+print("Greatest Decrease in Revenue:", MinRevChangeDate, "($",MinRevChange,")")
+with open("outputfile.txt",'w') as op:
+        op.write("Financial Analysis:\n--------------------------------------------------\n")
+        op.write("Total Months: "+str(len(Date))+"\n")
+        op.write("Total Revenue: $"+str(sum(Revenue))+"\n")
+        op.write("Average Change: $"+AvgRevChange+"\n")
+        op.write("Greatest Increase In Profits: "+MaxRevChangeDate+"($"+MaxRevChange+")"+"\n")
+        op.write("Greatest decrease in profits: "+MinRevChangeDate+"($"+MinRevChange+")"+"\n")
+
+
+
+
+
+
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,23 +1,36 @@
 import os 
 import csv
-VoterID = []
-County = []
-Candidate = []
-#path
-pollCSV = os.path.join("Resources", "election_data.csv")
-#function
-def election(voterData):
-    print(f"Total vote: {str(totalVote)}")
-#read
+pollCSV = os.path.join("election_data.csv")
 with open(pollCSV, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
+    VoterID = []
+    County = []
+    Candidate = []
+    Candidate_choice = []
     for row in csvreader:
         VoterID.append(row[0])
         County.append(row[1])
         Candidate.append(row[2])
-        totalVote = len(Candidate)
-election
-
-
-#print
+print("Election Results")
+print("-------------------------------------------")
+print("Total Votes: ", len(VoterID)) 
+print("-------------------------------------------")
+for i in set(Candidate):
+    Candidate_choice.append(Candidate.count(i))
+Combined = dict(zip(set(Candidate), Candidate_choice))
+sortedCandidate = (sorted(Combined.items(), key=lambda x: x[1], reverse=True))
+with open("outputfile.txt",'w') as op:
+    op.write("Election Results\n--------------------------------------------------\n")
+    op.write("Total Votes: " + str(len(VoterID)) + "\n--------------------------------------------------\n")
+    for i in range(len(sortedCandidate)):
+        Name = str(sortedCandidate[i][0])
+        Num = sortedCandidate[i][1]
+        Total = len(VoterID)
+        Percentage = round(((float(Num)/Total) * 100),3)
+        print(Name + ": " + str(Percentage) + "%" + " (" + str(Num) + ")")
+        op.write(str(sortedCandidate[i][0]) + ":" + str(round(((float(sortedCandidate[i][1])/Total) * 100),3)) + "%" + "(" + str(sortedCandidate[i][1]) + ")\n")
+    print("-------------------------------------------")
+    print("Winner: " + str(sortedCandidate[0][0]))
+    print("-------------------------------------------")
+    op.write("--------------------------------------------------\n" + "Winner: " + str(sortedCandidate[0][0]) + "\n--------------------------------------------------\n")
